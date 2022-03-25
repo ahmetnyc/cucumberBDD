b0be1facc4406cd1be2d67a110f69713c55f0032
ahmetnyc <ahmet.arslan.nyc@gmail.com>
2022-03-25 00:46:49 -0400

    new

--- src/test/java/renastech/pages/Guruhome.java
+++ src/test/java/renastech/pages/Guruhome.java
@@ -8,23 +8,44 @@ import renastech.utils.BrowserUtils;
 
 public class Guruhome extends BrowserUtils {
     public Guruhome(){PageFactory.initElements(driver,this);  }
+    @FindBy(xpath="//a[.='Add Customer']")
+    private WebElement addCostumer;
     @FindBy(xpath = "//h1[.='Add Customer']")
-    private WebElement logo;
-    @FindBy(xpath = "//a[.='Add Customer']")
-    private WebElement addcustomer;
-    @FindBy(xpath = "//h1[.='Add Customer']")
-    private WebElement verifymessage;
-    @FindBy(id = "//input[@id='done']")
+    private WebElement header2;
+    @FindBy(xpath="//label[@for='done']")
     private WebElement doneButton;
-    @FindBy(id = "fname")
-    private WebElement fname;
-    @FindBy(id = "lname")
-    private WebElement lastName;
+    @FindBy(xpath="//label[@for='pending']")
+    private WebElement pendingButton;
+    @FindBy(id="fname")
+    private WebElement fName;
+    @FindBy(id="lname")
+    private WebElement lName;
+    @FindBy(id="email")
+    private WebElement emailID;
+    @FindBy(name="addr")
+    private WebElement addressBox;
+    @FindBy(id="telephoneno")
+    private WebElement pNumber;
 
-    public void setLogo(String Header){Assert.assertEquals(Header,logo.getText());}
-    public void setAddcustomer(){clickWithWait(addcustomer);}
-    public void setVerifymessage(String expectedHeader){Assert.assertEquals(expectedHeader,verifymessage.getText());}
-    public void setDone(){clickWithWait(doneButton);}
-    public void setName(String Fname){enterText(fname,Fname);}
-    public void setLastName(String Lname){enterText(lastName,Lname); }
+
+    public void setAddCostumer(){clickWithWait(addCostumer);}
+    public void setHeader2(String expectedheader2){Assert.assertEquals(expectedheader2,header2.getText());}
+    public void setDoneButton(String DoneButton){clickWithWait(doneButton);}
+    public void setPendingButton(String PendingButton){clickWithWait(pendingButton);}
+
+    public void setBackgroundCheck (String Background){
+        if (Background.equalsIgnoreCase(pendingButton.getText())){
+
+            pendingButton.click();
+        } else {
+            doneButton.click();
+        }
+    }
+    public void setFName(String fname){enterText(fName,fname);}
+    public void setLName(String lname){enterText(lName,lname);}
+    public void setEmailID(String email){enterText(emailID,email);}          //{emailID.sendKeys(email);}
+    public void setAddressBox(String address){enterText(addressBox,address);}
+    public void setPNumber(String number){enterText(pNumber,number);}
+
 }
+
--- src/test/java/renastech/stepDefinitions/Gurusteph.java
+++ src/test/java/renastech/stepDefinitions/Gurusteph.java
@@ -48,21 +48,25 @@ public class Gurusteph extends BrowserUtils {
 
     @When("The user wants to add a customer")
     public void the_user_wants_to_add_a_customer() {
-       guruhome.setAddcustomer();
+       guruhome.setAddCostumer();
     }
     @Then("The users wants to verify {string} header")
-    public void the_users_wants_to_verify_header(String Header) {
-       guruhome.setLogo(Header);
-    guruhome.setVerifymessage(Header);
+    public void the_users_wants_to_verify_header(String expectedheader2) {
+       guruhome.setHeader2(expectedheader2);
+
     }
     @Then("The user wants to enter background check as {string}")
-    public void the_user_wants_to_enter_background_check_as(Map<String,String> datatable) {
-    guruhome.setDone();
-    guruhome.setName(datatable.get("Joe"));
-    guruhome.setLastName(datatable.get("Dave"));
+    public void the_user_wants_to_enter_background_check_as(String Background)  {
+       guruhome.setBackgroundCheck(Background);
     }
     @Then("The user wants to enter billingaddress as {string} and {string} and {string} and {string} and {string}")
-    public void the_user_wants_to_enter_billingaddress_as_and_and_and_and() {
+    public void the_user_wants_to_enter_billingaddress_as_and_and_and_and(String fname,String lname, String email,String address,String number) {
+       guruhome.setFName(fname);
+       guruhome.setLName(lname);
+       guruhome.setEmailID(email);
+       guruhome.setAddressBox(address);
+       guruhome.setPNumber(number);
+
     }
     @Then("The wants to verify message as {string}")
     public void the_wants_to_verify_message_as(String string) {
